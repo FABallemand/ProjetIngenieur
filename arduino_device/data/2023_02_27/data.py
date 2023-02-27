47a581efa696315acf9b6f3b97e24ad09b84fb1f
FABallemand <allemand.fabien@orange.fr>
2023-02-27 11:08:32 +0100

    Data visualisation

--- arduino_device/data/2023_02_27/data.py
+++ arduino_device/data/2023_02_27/data.py
@@ -12,5 +12,18 @@ nb_files = 4
 data = []
 
 for i in range(nb_files):
-    data.append(pd.read_csv("DATA" + str(i+1) + ".TXT"))
-    print(data[i])
+    data = pd.read_csv("DATA" + str(i+1) + ".TXT", sep=";", header=None, names=["ax", "ay", "az"])
+    nb_measure = data.shape[0]
+
+    fig, axs = plt.subplots(1,3)
+    fig.set_tight_layout(True)
+    fig.set_size_inches(12, 4)
+
+    axs[0].plot(range(nb_measure), data["ax"])
+    axs[0].set_title("Acceleration X")
+    axs[1].plot(range(nb_measure), data["ay"])
+    axs[1].set_title("Acceleration Y")
+    axs[2].plot(range(nb_measure), data["az"])
+    axs[2].set_title("Acceleration Z")
+
+    fig.savefig("DATA" + str(i+1))
