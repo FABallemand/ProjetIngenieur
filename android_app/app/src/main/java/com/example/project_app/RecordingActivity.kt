40cb79b906f8552b2842f81299683a05339e671f
FABallemand <allemand.fabien@orange.fr>
2023-04-03 09:43:56 +0200

    Setup new Android Studio project

---
+++ android_app/app/src/main/java/com/example/project_app/RecordingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.project_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recording)
+    }
+}
