33df2f81b96f90cf716cbe3398745f73ba8b73e3
FABallemand <allemand.fabien@orange.fr>
2023-04-18 10:23:34 +0200

    Data analysis

---
+++ data/android_app/2023_04_14/plot.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(path)
+
+def single_graph(df):
+    fig = go.Figure()
+    for col in df.columns:
+      fig.add_trace(go.Scatter(x=df.index,y=df[col],name=col))
+    return fig
+
+def triple_graph(df):
+    col_num = 1
+    fig = make_subplots(rows=1, cols=3)
+    for col in df.columns:
+      fig.add_trace(go.Scatter(x=df.index,y=df[col],name=col), row=1, col=col_num)
+      col_num += 1
+    fig.update_layout(height=1080/2, width=1920/2, title_text="Test")
+    return fig
+
+files = [file for file in os.listdir(path) if os.path.isfile(file)]
+files.remove("plot.py")
+print(files)
+
+for file in files:
+    acc_data = pd.read_csv(file, sep=";", header=1, names=["ay", "ax", "az"])
+    fig = single_graph(acc_data[["ax", "ay", "az"]])
+    fig.write_html(file + ".html", full_html=False, include_plotlyjs="cdn")
