52a2fcbab4833fc12ac3a8d0cbc0402d3634d93f
FABallemand <allemand.fabien@orange.fr>
2023-05-14 23:15:50 +0200

    #4 Rework data augmentation

--- data/android_app/2023_04_14/plot.py
+++ data/android_app/2023_04_14/plot.py
@@ -6,13 +6,13 @@ from plotly.subplots import make_subplots
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 
-def single_graph(df):
+def singleGraph(df):
     fig = go.Figure()
     for col in df.columns:
       fig.add_trace(go.Scatter(x=df.index,y=df[col],name=col))
     return fig
 
-def triple_graph(df):
+def tripleGraph(df):
     col_num = 1
     fig = make_subplots(rows=1, cols=3)
     for col in df.columns:
@@ -27,5 +27,5 @@ print(files)
 
 for file in files:
     acc_data = pd.read_csv(file, sep=";", header=1, names=["ay", "ax", "az"])
-    fig = single_graph(acc_data[["ax", "ay", "az"]])
+    fig = singleGraph(acc_data[["ax", "ay", "az"]])
     fig.write_html(file + ".html", full_html=False, include_plotlyjs="cdn")
